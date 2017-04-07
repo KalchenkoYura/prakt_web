4ff1f4cda864117ddcd61f861e2a5a91ae7e55b7
KalchenkoYura <yura.kalchenko@mail.ru>
2017-04-07 12:19:09 +0300

    Add files via upload

--- pr15.php
+++ pr15.php
@@ -1,11 +1,15 @@
 <?php
 function isNum ($value)
 {
+	if(gettype($value)==gettype("string")){
+		$value = preg_replace("/[^0-9]/", '', $value);
+		echo (float)($value) . "<br>";
+	}else{
     if (is_numeric($value)) {
-        echo gettype($value) . "<br>";
+        echo (float)($value) . "<br>";
     } else {
         echo 'false';
-    }
+    }}
 }
 $testNum = array("12", 9.75, 11, "IgorHeal03");
 foreach ($testNum as $value)
